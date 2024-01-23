d78e0206cef7ced45406d6c84977ca3ac0a2b214
Abu Chandy Abraham <abu.chandyabraham@cognizant.com>
2024-01-23 17:19:28 +0000

    snapshots

---
+++ snapshots/scd_raw_customer.sql
@@ -0,0 +1,13 @@
+{% snapshot scd_raw_listing %}
+    {{
+        config(
+            target_schema="dbt_achandyabraham",
+            unique_key="id",
+            strategy="timestamp",
+            updated_at="updated_at",
+            invalidate_hard_deletes=True,
+        )
+    }}
+    select *
+    from airbnb.raw.raw_listings
+{% endsnapshot %}
